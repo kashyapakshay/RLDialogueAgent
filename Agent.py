d284631559e0605c262057f299f34da3a577a8d9
Akshay Kashyap <kashyapa@union.edu>
2016-10-23 23:12:21 -0400

    Implemented Agent for learning intervention

--- Agent.py
+++ Agent.py
@@ -1,31 +1,26 @@
 import itertools
+import nump as np
 
 from QLearner import QLearner
 
 class BaseAgent:
-    def __init__(self, stateTemplate=(), statesMap={}, actions=[], rewardMatrix=[], initialState=None):
+    def __init__(self, states=[], actions=[], rewardMatrix=[], initialState=None):
         self._currentState = initialState
-
-        self._stateTemplate = stateTemplate
 
         # statesMap ={
         #     state1: ['value1', 'value2']
         #     state2: ['value1', 'value2', 'value3']
         #     ...
         # }
-        self._statesMap = statesMap
 
+        self._states = states
         self._actions = actions
 
         self.qLearner = QLearner(
-            states=self._generateStateSpace(self._statesMap, self._stateTemplate), \
+            states=self._states, \
             actions=self._actions, \
             rewardMatrix=rewardMatrix \
         )
-
-    def _generateStateSpace(self, statesMap, stateTemplate):
-        stateValuesList = [statesMap[state] for state in stateTemplate]
-        return itertools.product(*stateValuesList)
 
     def getCurrentState(self):
         return self._currentState
@@ -55,5 +50,37 @@ class InstructionGenerationAgent(BaseAgent):
         self.rewardMatrix = []
         self.initialState = ()
 
-        BaseAgent.__init__(stateTemplate=self._stateTemplate, statesMap=self.statesMap, \
+        BaseAgent.__init__(states=self.generateStateSpace(self.statesMap, self.stateTemplate), \
             actions=self.actions, rewardMatrix=self.rewardMatrix, initialState=self.initialState)
+
+        def generateStateSpace(self, statesMap, stateTemplate):
+            stateValuesList = [statesMap[state] for state in stateTemplate]
+            return itertools.product(*stateValuesList)
+
+class InterventionAgent(BaseAgent):
+    def __init__(self):
+        self._stateTemplate = ('current-follower-action', 'task-trajectory', 'last-action')
+
+        self.statesMap = {
+            'current-follower-action': ['working', 'clarifiying', 'no-action'],
+            'task-trajectory': ['closer', 'further', 'no-change', 'finished'],
+            'last-action': ['instruction-given', 'instruction-clarified', 'instruction-followed', 'no-action']
+        }
+
+        self.actions = ['intervene', 'no-intervene']
+
+        self.initialState = ('no-action', 'no-change', 'no-action')
+        self.states = self.generateStateSpace(self.statesMap, self.stateTemplate)
+
+        self.rewardMatrix = np.array([[0 for state in self._states] for state in self._states])
+
+        for state in self.states:
+            for finishState in itertools.product(statesMap['current-follower-action'], ['finished'], statesMap['last-action']):
+                self.rewardMatrix[state, finishState] = 100
+
+        BaseAgent.__init__(states=self.states, \
+            actions=self.actions, rewardMatrix=self.rewardMatrix, initialState=self.initialState)
+
+    def generateStateSpace(self, statesMap, stateTemplate):
+        stateValuesList = [statesMap[state] for state in stateTemplate]
+        return itertools.product(*stateValuesList)
